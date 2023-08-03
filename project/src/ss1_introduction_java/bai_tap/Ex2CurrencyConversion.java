5e74050e32686a020fd54f683e62c27d98791de0
Quang-Truong-2001 <quangtruong2112001@gmail.com>
2023-08-03 10:37:54 +0700

    fix ss01

--- project/src/ss1_introduction_java/bai_tap/Ex2CurrencyConversion.java
+++ project/src/ss1_introduction_java/bai_tap/Ex2CurrencyConversion.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 
 public class Ex2CurrencyConversion {
     public static void main(String[] args) {
-        double vnd = 23000;
+        double convert = 23000;
         double usd;
         Scanner sc = new Scanner(System.in);
         System.out.println("Mời bạn nhập số tiền USD: ");
         usd = sc.nextDouble();
-        double convert = usd * vnd;
-        System.out.println("Giá trị VND: " + convert);
+        double vnd = usd * convert;
+        System.out.println("Giá trị VND: " + vnd);
     }
 }
